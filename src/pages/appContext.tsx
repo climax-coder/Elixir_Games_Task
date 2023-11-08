5f01f83aeae759746a639ce3afd13c99591acb63
seniorprodev <seniorprodev@gmail.com>
2023-11-08 20:49:03 +0200

    fix error

--- src/pages/appContext.tsx
+++
@@ -1,9 +0,0 @@
-import React, { createContext, useContext } from 'react'
-
-type AppContextValue = {
-  colorScheme: 'light' | 'dark';
-  onChange: (color: 'light' | 'dark') => void;
-};
-
-export const AppContext = createContext<AppContextValue | null>(null);
-export const useAppContext = () => useContext(AppContext);
